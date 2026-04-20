a0bad6e747d75c09bed490668a55977e2f6d75d6
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2026-04-20 20:54:39 +0300

    fix : update footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -34,21 +34,21 @@
               <svg class="social-icon" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56-.35-.12-.74-.03-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z"/>
               </svg>
-              <span>+261 63 648 85</span>
+              <span>+261 34 63 648 85</span>
             </a>
             <a href="https://github.com/Sandratra05" target="_blank" rel="noopener noreferrer" class="social-link">
               <i class="devicon-github-original"></i>
-              <span>GitHub</span>
+              <span>Sandratra05</span>
             </a>
             <a href="https://linkedin.com/in/sandratra-raben" target="_blank" rel="noopener noreferrer" class="social-link">
               <i class="devicon-linkedin-plain"></i>
-              <span>LinkedIn</span>
+              <span>Sandratra Rabenanahary</span>
             </a>
             <a href="https://www.facebook.com/Sandratra.rabenanahary/" target="_blank" rel="noopener noreferrer" class="social-link">
               <svg class="social-icon" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
               </svg>
-              <span>Facebook</span>
+              <span>Sandratra Rabenanahary</span>
             </a>
           </div>
         </div>
